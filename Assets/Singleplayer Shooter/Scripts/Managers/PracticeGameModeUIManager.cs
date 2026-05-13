e8fe638b99fc5bb734db05d93b4bef77e71b5325
Manu <93039866+manu-nr@users.noreply.github.com>
2026-05-14 00:38:55 +0530

    Timer implementation

--- Assets/Singleplayer Shooter/Scripts/Managers/PracticeGameModeUIManager.cs
+++ Assets/Singleplayer Shooter/Scripts/Managers/PracticeGameModeUIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -7,8 +8,36 @@ public class PracticeGameModeUIManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _currentGun;
     [SerializeField] private TextMeshProUGUI _currentGunAmmoText;
     [SerializeField] private TextMeshProUGUI _totalAmmo;
+    [SerializeField] private TextMeshProUGUI _timerText;
 
+    private float _timer;
+    private bool _runTimer;
 
+    #region Unity Methods
+    private void Start()
+    {
+        GameModeManager.OnGameModeStateChange += HandleGameModeStateChange;
+    }
+
+    private void OnDestroy()
+    {
+        GameModeManager.OnGameModeStateChange -= HandleGameModeStateChange;
+    }
+
+    private void Update()
+    {
+        if(_runTimer)
+        {
+            _timer += Time.deltaTime;
+            int seconds = Mathf.FloorToInt(_timer % 60);
+            int minutes = Mathf.FloorToInt(_timer / 60);
+
+            _timerText.SetText(string.Format("{0:00}:{1:00}", minutes, seconds));
+        }
+    }
+    #endregion
+
+    #region Public Methods
     public void UpdateDamageablesUI(int totalDamageables, int remainingDamageables)
     {
         _remainingdamageablesCountText.SetText($"{remainingDamageables}/{totalDamageables}");
@@ -16,9 +45,33 @@ public class PracticeGameModeUIManager : MonoBehaviour
 
     public void UpdateWeaponsUI(WeaponData data)
     {
-        Debug.Log("[NRM] After switch Current ammo: " + WeaponManager.Instance.CurrentWeapon.CurrentAmmo);
         _currentGun.SetText("Gun: " + data.weaponName);
         _currentGunAmmoText.SetText($"Ammo: {WeaponManager.Instance.CurrentWeapon.CurrentAmmo} / {data.magazineSize}");
         _totalAmmo.SetText($"{WeaponManager.Instance.CurrentWeapon.TotalAmmo}");
     }
+    #endregion
+
+    #region Private Methods
+    private void HandleGameModeStateChange(ModeType type, bool isStarted)
+    {
+        if(type == ModeType.PRACTICE_MODE)
+        {
+            if (isStarted)
+                StartTimer();
+            else
+                StopTimer();
+        }
+    }
+
+    private void StartTimer()
+    {
+        _timer = 0f;
+        _runTimer = true;
+    }
+
+    private void StopTimer()
+    {
+        _runTimer = false;
+    }
+    #endregion
 }
